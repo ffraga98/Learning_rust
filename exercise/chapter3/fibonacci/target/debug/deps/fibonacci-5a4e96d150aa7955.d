f3f365f546d59a4fa4476197e330d5b258a0d73f
ffraga98 <ferfraga2@gmail.com>
2022-08-10 12:36:26 -0300

    Removing target repository

--- exercise/chapter3/fibonacci/target/debug/deps/fibonacci-5a4e96d150aa7955.d
+++
@@ -1,5 +0,0 @@
-/home/fernando/Documentos/rust_projects/Learning_rust/exercise/chapter3/fibonacci/target/debug/deps/fibonacci-5a4e96d150aa7955: src/main.rs
-
-/home/fernando/Documentos/rust_projects/Learning_rust/exercise/chapter3/fibonacci/target/debug/deps/fibonacci-5a4e96d150aa7955.d: src/main.rs
-
-src/main.rs:
--- exercise/chapter3/fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-/home/fernando/Documentos/rust_projects/Learning_rust/exercise/chapter3/fibonacci/target/debug/fibonacci: /home/fernando/Documentos/rust_projects/Learning_rust/exercise/chapter3/fibonacci/src/main.rs
