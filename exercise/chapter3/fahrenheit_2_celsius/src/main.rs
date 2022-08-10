5a658c27a1fd00710f4ebe42c8341411558ca84e
ffraga98 <ferfraga2@gmail.com>
2022-08-10 11:52:17 -0300

    ADD: Chapter 2: Guessing game. Chapter 3: F° to C°

---
+++ exercise/chapter3/fahrenheit_2_celsius/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() {
+    loop {
+    println!("Type the temperature in Fahrenheit scale");
+    
+    let mut temperature = String::new();
+        
+    read_user_input(&mut temperature);
+    
+    if temperature.trim() == "q" {
+        break; 
+    }
+
+    let temperature: f64 = match temperature.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+    let temperature = fahrenheit_2_celsius(temperature);
+    println!("The temperature in Celsius scale is: {temperature}");
+    }
+}
+
+fn fahrenheit_2_celsius( t : f64 ) -> f64 {
+    ( t - 32.0 ) * 5.0 / 9.0
+}
+
+fn read_user_input( input : &mut String) {
+    io::stdin().read_line( input ).expect("Please type a number!");
+}
+
