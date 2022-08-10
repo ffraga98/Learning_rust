58303cebf8f5aa6a1a7dc4dcf7c59ad4c5888494
ffraga98 <ferfraga2@gmail.com>
2022-08-10 12:32:53 -0300

    ADD: print EXIT

--- exercise/chapter3/fahrenheit_2_celsius/src/main.rs
+++ exercise/chapter3/fahrenheit_2_celsius/src/main.rs
@@ -2,23 +2,25 @@ use std::io;
 
 fn main() {
     loop {
-    println!("Type the temperature in Fahrenheit scale");
+        println!("Type the temperature in Fahrenheit scale. [EXIT = q ]");
     
-    let mut temperature = String::new();
+        let mut temperature = String::new();
         
-    read_user_input(&mut temperature);
+        read_user_input(&mut temperature);
     
-    if temperature.trim() == "q" {
-        break; 
-    }
+        if temperature.trim() == "q" {
+            break; 
+        }
 
-    let temperature: f64 = match temperature.trim().parse(){
-        Ok(num) => num,
-        Err(_) => continue,
-    };
+        let temperature: f64 = match temperature.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let temperature = fahrenheit_2_celsius(temperature);
-    println!("The temperature in Celsius scale is: {temperature}");
+        let temperature = fahrenheit_2_celsius(temperature);
+
+        println!("The temperature in Celsius scale is: {temperature}");
+    
     }
 }
 
